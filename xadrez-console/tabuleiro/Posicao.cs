495fb88d7eec14e15f8292ab3306a908c7dd149a
VictorSamp <victor.msampaio@outlook.com.br>
2021-02-12 14:24:22 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -2,6 +2,7 @@
 {
     class Posicao
     {
+
         public int linha { get; set; }
         public int coluna { get; set; }
 
@@ -11,11 +12,17 @@
             this.coluna = coluna;
         }
 
+        public void definirValores(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
         public override string ToString()
         {
             return linha
-                   + ", "
-                   + coluna;
+                + ", "
+                + coluna;
         }
     }
 }
